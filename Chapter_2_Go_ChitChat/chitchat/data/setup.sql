97477c34d3cad88f6f19b663598fb7be8a5b4674
cucy <292016176@qq.com>
2018-03-06 00:03:06 +0800

    Update setup.sql

--- Chapter_2_Go_ChitChat/chitchat/data/setup.sql
+++ Chapter_2_Go_ChitChat/chitchat/data/setup.sql
@@ -1,39 +1,90 @@
-drop table posts;
-drop table threads;
-drop table sessions;
-drop table users;
+-- drop table posts;
+-- drop table threads;
+-- drop table sessions;
+-- drop table users;
 
 
-create table users (
-  id         serial primary key,
-  uuid       varchar(64) not null unique,
-  name       varchar(255),
-  email      varchar(255) not null unique,
-  password   varchar(255) not null,
-  created_at timestamp not null   
-);
+CREATE SCHEMA IF NOT EXISTS `chitchat` DEFAULT CHARACTER SET utf8 ;
+USE `chitchat` ;
 
-create table sessions (
-  id         serial primary key,
-  uuid       varchar(64) not null unique,
-  email      varchar(255),
-  user_id    integer references users(id),
-  created_at timestamp not null   
-);
+-- -----------------------------------------------------
+-- Table `chitchat`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chitchat`.`users` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uuid` VARCHAR(64) NOT NULL,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id` (`id` ASC),
+  UNIQUE INDEX `uuid` (`uuid` ASC),
+  UNIQUE INDEX `email` (`email` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-create table threads (
-  id         serial primary key,
-  uuid       varchar(64) not null unique,
-  topic      text,
-  user_id    integer references users(id),
-  created_at timestamp not null       
-);
 
-create table posts (
-  id         serial primary key,
-  uuid       varchar(64) not null unique,
-  body       text,
-  user_id    integer references users(id),
-  thread_id  integer references threads(id),
-  created_at timestamp not null  
-);
+-- -----------------------------------------------------
+-- Table `chitchat`.`threads`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chitchat`.`threads` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uuid` VARCHAR(64) NOT NULL,
+  `topic` TEXT NULL DEFAULT NULL,
+  `user_id` BIGINT(20) UNSIGNED NULL DEFAULT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id` (`id` ASC),
+  UNIQUE INDEX `uuid` (`uuid` ASC),
+  INDEX `user_id` (`user_id` ASC),
+  CONSTRAINT `threads_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `chitchat`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `chitchat`.`posts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chitchat`.`posts` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uuid` VARCHAR(64) NOT NULL,
+  `body` TEXT NULL DEFAULT NULL,
+  `user_id` BIGINT(20) UNSIGNED NULL DEFAULT NULL,
+  `thread_id` BIGINT(20) UNSIGNED NULL DEFAULT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id` (`id` ASC),
+  UNIQUE INDEX `uuid` (`uuid` ASC),
+  INDEX `user_id` (`user_id` ASC),
+  INDEX `thread_id` (`thread_id` ASC),
+  CONSTRAINT `posts_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `chitchat`.`users` (`id`),
+  CONSTRAINT `posts_ibfk_2`
+    FOREIGN KEY (`thread_id`)
+    REFERENCES `chitchat`.`threads` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `chitchat`.`sessions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chitchat`.`sessions` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uuid` VARCHAR(64) NOT NULL,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `user_id` BIGINT(20) UNSIGNED NULL DEFAULT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id` (`id` ASC),
+  UNIQUE INDEX `uuid` (`uuid` ASC),
+  INDEX `user_id` (`user_id` ASC),
+  CONSTRAINT `sessions_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `chitchat`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
